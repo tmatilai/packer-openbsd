d4c4f911fe7a14431896c3b2aa8c30cc13ba0825
Patrik Ragnarsson <patrik@starkast.net>
2015-10-18 22:43:34 +0200

    Add OpenBSD 5.8 templates
    sudo has moved to ports and installer changes (from
    http://www.openbsd.org/plus58.html):
    
    > In the installer, rework sshd enable root login questions in light of
    > sshd PermitRootLogin default change. The new default is not to ask to
    > enable root logins when a non-root user has been added.
    
    > In the installer, make "without-password" the default answer to the
    > "Allow root ssh login?" question.

---
+++ scripts-5.8/ansible.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+if [ x$VAGRANT_ANSIBLE = x'yes' ]; then
+	. /root/.profile
+
+	uname_r=`uname -r`
+	if [ $uname_r = 5.8 ]; then
+		pkg_add python-2.7.10
+	elif [ $uname_r = 5.7 ]; then
+		pkg_add python-2.7.9
+	elif [ $uname_r = 5.6 ]; then
+		pkg_add python-2.7.8
+	elif [ $uname_r = 5.5 ]; then
+		pkg_add python-2.7.6p0
+	elif [ $uname_r = 5.4 ]; then
+		pkg_add python-2.7.5
+	else
+		echo "Did not recognise OpenBSD version."
+		exit 1
+	fi
+else
+	echo "Building a box without Python 2 (Ansible)"
+fi
