e8703e70f7262c5f5db9be5e1ff39a773d1dbf18
Matt Behrens <matt@zigg.com>
2015-12-08 20:06:22 -0500

    remove wget and curl dependencies

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,14 +4,13 @@ set -e
 
 uname_r=`uname -r`
 
-export PKG_PATH="$MIRROR/pub/OpenBSD/${uname_r}/packages/`arch -s`/"
+export PKG_PATH="$MIRROR/pub/OpenBSD/$uname_r/packages/`arch -s`/"
 
 # set pkg path for users
 echo "export PKG_PATH=\"$PKG_PATH\"" >> /root/.profile
 echo "export PKG_PATH=\"$PKG_PATH\"" >> /home/vagrant/.profile
 
-# install wget, curl, and sudo on 5.8
-pkg_add wget curl
+# install sudo on 5.8, required by Vagrant 1.7.4
 if [ $uname_r = 5.8 ]; then
     pkg_add sudo--
 fi
--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -3,8 +3,8 @@
 set -e
 
 mkdir -p /home/vagrant/.ssh
-wget --no-check-certificate \
-    'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
-    -O /home/vagrant/.ssh/authorized_keys
+cat <<EOT >/home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key
+EOT
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
